7dc9330e7477c6ff5c15ac1fa3da19e898ab4d6c
Allen Clark <allenclark0321@gmail.com>
2024-03-02 20:23:58 -0600

    minor changes

--- lib/opening_page.dart
+++ lib/opening_page.dart
@@ -5,13 +5,6 @@ class QuizMeOpeningPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
